a7ce8292d6165f0cd5e0297460a51adcda2830f9
Martin <joewwi03@gmail.com>
2023-05-22 12:19:46 +0300

    Completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence_fragments = self.split(/[.!?]/)
+    sentence_fragments.reject! { |fragment| fragment.empty? }
+    sentence_fragments.count
   end
 end
+
+binding.pry
+0
